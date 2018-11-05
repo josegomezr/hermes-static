46d3ce5b652c0cd8b8f2fb6c053c247d79fe50c4
Jose D. Gomez R <1josegomezr@gmail.com>
2018-11-05 17:13:28 -0400

    backgrounds

--- js/main.js
+++ js/main.js
@@ -125,7 +125,7 @@ $(".carousel-item[data-background-image]").each(function () {
 
   $el.css('backgroundImage', 'url(' + bgImg + ')');
   $el.css('backgroundPosition', 'center');
-  $el.css('backgroundSize', 'contain')
+  $el.css('backgroundSize', '100% 100%')
   $el.css('backgroundRepeat', 'no-repeat');
 
   $el.addClass('holderjs');
